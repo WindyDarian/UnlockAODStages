a4cc29761f02b81f50dbe6dfa4e17db5aec34a16
Windy Darian <p123456638@msn.com>
2016-01-09 15:54:18 -0500

    Final commit
    (escape

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace UnlockAODStages
+{
+
+    class Program
+    {
+        static void saveData(Object target, string file)
+        {
+            var fs = new FileStream(file, FileMode.Create);
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(fs, target);
+            fs.Close();
+        }
+        static void Main(string[] args)
+        {
+            List<int> enabledStages = Enumerable.Range(0, 100).ToList();
+            saveData(enabledStages, "Es.aod");
+            // and copy it to the "save" folder...
+        }
+    }
+}
